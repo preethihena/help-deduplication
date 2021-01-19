f638afaf158934b9656216f88d4bac2138f571c7
Hena Dindigalla <preethihena.22@gmail.com>
2021-01-19 22:05:17 +0530

    Add hashcode method and refactor equal method

--- src/main/java/com/dhruvk/point/Point.java
+++ src/main/java/com/dhruvk/point/Point.java
@@ -1,5 +1,7 @@
 package com.dhruvk.point;
 
+import java.util.Objects;
+
 public class Point {
     private final int x;
     private final int y;
@@ -14,8 +16,16 @@ public class Point {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Point point = (Point) o;
-        return x == point.x &&
-                y == point.y;
+        if(this.hashCode() == point.hashCode()) return true;
+        return false;
+
     }
 
+    @Override
+    public int hashCode() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(this.x);
+        buffer.append(this.y);
+        return buffer.toString().hashCode();
+    }
 }
